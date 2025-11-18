cc7807a5a3b5addd038e95e341c88961e5ed0094
matteolpera <matteo.lepera02@gmail.com>
2025-11-18 16:12:37 +0100

    feat: card aggiunte dinamicamente

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Ragionamento:
+// tramite chiamata api dall'array di oggetti devo prelevare:
+//titale, date e url e aggiungerle alla pagina html.
+// mi servirà sicuramente un ciclo per prelevare tutti gli oggetti.
+// per inserirli dinamicamente nella pagina userò il create element.
+
+
+//DOM
+const photoGrid = document.getElementById("photo-grid");
+// console.log(photoGrid);
+
+//chiamata
+
+axios
+    .get("https://lanciweb.github.io/demo/api/pictures/")
+    .then((resp) => {
+        const photoArray = resp.data;
+
+        photoArray.forEach(curPhoto => {
+            const colPhoto = document.createElement("div");
+            colPhoto.classList.add("col");
+
+            colPhoto.innerHTML = `<div class="card py-10">
+                    <div class="card-body">
+                        <div class="pin"><img src="./img/pin.svg" alt="" height="30"></div>
+                        <img src=${curPhoto.url} alt="">
+                        <p> <span>${curPhoto.date}</span><br>
+                            <span>${curPhoto.title.toUpperCase()}</span>
+                        </p>
+                    </div>
+            `
+            photoGrid.append(colPhoto);
+        });
+    })
